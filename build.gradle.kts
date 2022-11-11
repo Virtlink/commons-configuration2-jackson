4f623fd3402815e427e3dc2dc043fc373221a20d
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2022-11-11 11:45:10 +0100

    Suppress Intellij spurious errors

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,10 @@
+// Temporary fix for IntelliJ issue where `libs` is errored: https://youtrack.jetbrains.com/issue/KTIJ-19369
+@Suppress(
+    "DSL_SCOPE_VIOLATION",
+    "MISSING_DEPENDENCY_CLASS",
+    "UNRESOLVED_REFERENCE_WRONG_RECEIVER",
+    "FUNCTION_CALL_EXPECTED"
+)
 plugins {
     `java-library`
     `maven-publish`
