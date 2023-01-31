6afdd8d2c63cf3b182c35091f5e60af9abeb0595
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2023-01-31 11:16:39 +0100

    Remove most supressions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,5 @@
-// Temporary fix for IntelliJ issue where `libs` is errored: https://youtrack.jetbrains.com/issue/KTIJ-19369
-@Suppress(
-    "DSL_SCOPE_VIOLATION",
-    "MISSING_DEPENDENCY_CLASS",
-    "UNRESOLVED_REFERENCE_WRONG_RECEIVER",
-    "FUNCTION_CALL_EXPECTED"
-)
+// Workaround for IntelliJ issue where `libs` is errored: https://youtrack.jetbrains.com/issue/KTIJ-19369
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     `java-library`
     `maven-publish`
